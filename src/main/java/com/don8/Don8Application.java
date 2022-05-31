32bf312737ee8097ba56a48f137737e1d0871402
Jaydeep <jaydeepdharamsey21@gmail.com>
2022-05-31 07:49:16 -0400

    initial commit

---
+++ src/main/java/com/don8/Don8Application.java
@@ -0,0 +1,13 @@
+package com.don8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Don8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Don8Application.class, args);
+	}
+
+}
