1aefcc389f97e66ee9ac32a7e5debf84cd3ecc1e
Jaydeep <jaydeepdharamsey21@gmail.com>
2022-07-22 11:37:09 -0400

    removed auth for image urls

--- src/main/java/com/don8/config/WebSecurityConfig.java
+++ src/main/java/com/don8/config/WebSecurityConfig.java
@@ -127,6 +127,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/user/profile/**").permitAll()
+                .antMatchers("/api/product/image/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
